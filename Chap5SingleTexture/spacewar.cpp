bda911ea4519ab1f89fb476e868a4d6bd460100e
dbrowndan <BROWNDE1@2CE328120F.GCC.edu>
2015-10-05 09:34:29 -0400

    Initial commit

---
+++ Chap5SingleTexture/spacewar.cpp
@@ -0,0 +1,167 @@
+// Programming 2D Games
+// Copyright (c) 2011 by: 
+// Charles Kelly
+// Draw planet with transparency
+// Chapter 5 spacewar.cpp v1.0
+// This class is the core of the game
+
+#include "spaceWar.h"
+
+//=============================================================================
+// Constructor
+//=============================================================================
+Spacewar::Spacewar() {
+	fishSpawnCount = 0;
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+Spacewar::~Spacewar()
+{
+    releaseAll();           // call onLostDevice() for every graphics item
+}
+
+//=============================================================================
+// Initializes the game
+// Throws GameError on error
+//=============================================================================
+void Spacewar::initialize(HWND hwnd)
+{
+    Game::initialize(hwnd); // throws GameError
+
+	if (!bkgTexture.initialize(graphics, BKG_IMAGE))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Bkg texture initialization failed"));
+	if (!bkg.initialize(graphics, 0,0,0, &bkgTexture))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Error init bkg"));
+	bkg.setX(0);
+	bkg.setY(0);
+	bkg.setScale(BKG_IMAGE_SCALE);
+
+	if (!boatTexture.initialize(graphics, BOAT_IMAGE))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Boat texture initialization failed"));
+	if (!boat.initialize(graphics, 0,0,0, &boatTexture))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Error init boat"));
+	boat.setX(GAME_WIDTH/2 - boatTexture.getWidth()*BOAT_IMAGE_SCALE/2);
+	boat.setY(0);
+	boat.setScale(BOAT_IMAGE_SCALE);
+
+	for (int i = 0; i < FISH_COUNT; i++) {
+		if (!fishTexture[i].initialize(graphics, FISH_IMAGE))
+			throw(GameError(gameErrorNS::FATAL_ERROR, "Fish texture initialization failed"));
+		if (!fish[i].initialize(graphics, 0,0,0, &fishTexture[i]))
+			throw(GameError(gameErrorNS::FATAL_ERROR, "Error init fish"));
+		fish[i].setScale(FISH_IMAGE_SCALE);
+
+		fishPos[i].xPos = fish[i].getX();
+		fishPos[i].yPos = fish[i].getY();
+		fishVel[i].xVel = 0;
+		fishVel[i].yVel = 0;
+	}
+
+	//Stuff for physics
+	boatPos.xPos = boat.getX();
+	boatPos.yPos = boat.getY();
+	
+	boatVel.xVel = 200;
+
+
+    return;
+}
+
+//=============================================================================
+// Update all game items
+//=============================================================================
+void Spacewar::update()
+{
+	// arrow keys move boat
+	if (input->isKeyDown(VK_LEFT)) boatPos.xPos = boat.getX() - boatVel.xVel*frameTime;
+	if (input->isKeyDown(VK_RIGHT)) boatPos.xPos = boat.getX() + boatVel.xVel*frameTime;
+	
+	// don't let the boat go off the screen
+	if (boatPos.xPos < 0) boatPos.xPos = 0;
+	if (boatPos.xPos + boatTexture.getWidth()*BOAT_IMAGE_SCALE > GAME_WIDTH) boatPos.xPos = GAME_WIDTH - boatTexture.getWidth()*BOAT_IMAGE_SCALE;
+
+	boat.setX(boatPos.xPos);
+
+	// randomly spawn fish
+	if (rand() % 10 < 5 && fishSpawnCount < FISH_COUNT) {
+		fishPos[fishSpawnCount].xPos = rand() % (GAME_WIDTH - fish[fishSpawnCount].getWidth());
+		fish[fishSpawnCount].setX(fishPos[fishSpawnCount].xPos);
+		fishPos[fishSpawnCount].yPos = GAME_HEIGHT;
+		fish[fishSpawnCount].setY(fishPos[fishSpawnCount].yPos);
+		fishVel[fishSpawnCount].yVel = 100;
+		fishSpawnCount++;
+	}
+
+
+	for (int i = 0; i < fishSpawnCount; i++) {
+		fishPos[i].yPos -= fishVel[i].yVel * frameTime;
+	}
+
+ ////////////////
+// INPUT MODS
+ ////////////////
+
+ int directionX = 0;
+ int directionY = 0;
+
+}
+
+//=============================================================================
+// Artificial Intelligence
+//=============================================================================
+void Spacewar::ai()
+{}
+
+//=============================================================================
+// Handle collisions
+//=============================================================================
+void Spacewar::collisions()
+{}
+
+//=============================================================================
+// Render game items
+//=============================================================================
+void Spacewar::render()
+{
+    graphics->spriteBegin();                // begin drawing sprites
+
+	bkg.draw();
+	boat.draw();
+	// fish.draw();
+	for (int i = 0; i <= fishSpawnCount; i++){
+		fish[i].draw();
+	}
+
+	
+
+    graphics->spriteEnd();                  // end drawing sprites
+}
+
+//=============================================================================
+// The graphics device was lost.
+// Release all reserved video memory so graphics device may be reset.
+//=============================================================================
+void Spacewar::releaseAll()
+{
+	bkgTexture.onLostDevice();
+	boatTexture.onLostDevice();
+
+    Game::releaseAll();
+    return;
+}
+
+//=============================================================================
+// The grahics device has been reset.
+// Recreate all surfaces.
+//=============================================================================
+void Spacewar::resetAll()
+{
+   
+	bkgTexture.onResetDevice();
+	boatTexture.onResetDevice();
+
+    Game::resetAll();
+    return;
+}
---
+++ Chap5SingleTexture/spacewar.h
@@ -0,0 +1,75 @@
+// Programming 2D Games
+// Copyright (c) 2011 by: 
+// Charles Kelly
+// Chapter 5 spacewar.h v1.0
+
+#ifndef _SPACEWAR_H             // Prevent multiple definitions if this 
+#define _SPACEWAR_H             // file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include "game.h"
+#include "textureManager.h"
+#include "image.h"
+#include "graphics.h"
+
+//=============================================================================
+// This class is the core of the game
+//=============================================================================
+class Spacewar : public Game
+{
+private:
+
+	// My defines
+
+	#define SONY_IMAGE_SCALE 0.4
+	#define FISH_IMAGE_SCALE 1
+	#define BKG_IMAGE_SCALE 0.75
+	#define BOAT_IMAGE_SCALE 0.4
+
+// My vars
+
+	float boatXVel;
+
+	int fishSpawnCount;
+	TextureManager fishTexture[FISH_COUNT];
+	Image fish[FISH_COUNT];
+
+	TextureManager bkgTexture;
+	Image bkg;
+
+	TextureManager boatTexture;
+	Image boat;
+
+	struct position{
+		float xPos;
+		float yPos;
+		position() {xPos = 0; yPos = 0;}
+	} boatPos, fishPos[FISH_COUNT];
+
+	struct velocity{
+		float xVel;
+		float yVel;
+		velocity() { xVel = 0; yVel = 0;}
+	} boatVel, fishVel[FISH_COUNT];
+	
+public:
+    // Constructor
+    Spacewar();
+
+    // Destructor
+    virtual ~Spacewar();
+
+    // Initialize the game
+    void initialize(HWND hwnd);
+    void update();      // must override pure virtual from Game
+    void ai();          // "
+    void collisions();  // "
+    void render();      // "
+    void releaseAll();
+    void resetAll();
+};
+
+//Stuff for physics
+
+
+#endif
